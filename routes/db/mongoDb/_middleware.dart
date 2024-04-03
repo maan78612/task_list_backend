74c9422df2c8fafbd61d7b9b9a4df8ffec5cc67b
abdulrehman <maan852@live.com>
2024-04-03 15:09:30 +0500

    starting mongodb

---
+++ routes/db/mongoDb/_middleware.dart
@@ -0,0 +1,10 @@
+import 'package:dart_frog/dart_frog.dart';
+
+
+///  Command to generate middleware
+/// dart_frog new middleware "/db/mongoDb"
+
+Handler middleware(Handler handler) {
+  // TODO: implement middleware
+  return handler;
+}
